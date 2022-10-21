f3dca5fb11ff254409db9a6f092033ee0e4ad121
Treyceratops <treytowers@yahoo.com>
2022-10-20 17:34:18 -0700

    Update controller

--- controllers/groceryItemsController.js
+++ controllers/groceryItemsController.js
@@ -4,43 +4,57 @@ const GroceryItems = require('../models/groceryItem');
 const router = express.Router();
 
 // Index: Get all grocery items
-// http://localhost:8000/grocery-list/
-router.get('/grocery-list', (req, res, next) => {
-	GroceryItems.find({}).then((groceryItem) =>
-		res.status(200).json(groceryItem).catch(next)
-	);
+http://localhost:8000/grocery-list/
+router.get('/', async (req, res) => {
+	try {
+		res.json(await GroceryItems.find({}));
+	} catch (error) {
+		res.status(400).json(error);
+	}
 });
 
 // Show: Get one grocery item by id
 // http://localhost:8000/grocery-list/:id
-router.get('/grocery-list/:id', (req, res, next) => {
-	GroceryItems.findById({ _id: req.params.id }).then((groceryItem) =>
-		res.status(200).json(groceryItem).catch(next)
-	);
+router.get('/:id', async (req, res) => {
+	try {
+		res.json(await GroceryItems.findById({ _id: req.params.id }));
+	} catch (error) {
+		res.status(400).json(error);
+	}
 });
 
 // Create: Add a grocery item
 // http://localhost:8000/grocery-list/
-router.post('/grocery-list', (req, res, next) => {
-	GroceryItems.create(req.body).then((groceryItem) =>
-		res.status(201).json(groceryItem).catch(next)
-	);
+router.post('/', async (req, res) => {
+	try {
+		res.json(await GroceryItems.create(req.body));
+	} catch (error) {
+		res.status(400).json(error);
+	}
 });
 
 // Update: Edit a grocery item by id
 // http://localhost:8000/grocery-list/:id
-router.patch('/grocery-list/:id', (req, res, next) => {
-	GroceryItems.findByIdAndUpdate({ _id: req.params.id }, req.body, {
-		new: true,
-	}).then((groceryItem) => res.status(204).json(groceryItem).catch(next));
+router.patch('/:id', async (req, res) => {
+	try {
+		res.json(
+			await GroceryItems.findByIdAndUpdate({ _id: req.params.id }, req.body, {
+				new: true,
+			})
+		);
+	} catch (error) {
+		res.status(400).json(error);
+	}
 });
 
 // Delete: Remove a grocery item by id
 // http://localhost:8000/grocery-list/:id
-router.delete('/grocery-list/:id', (req, res, next) => {
-	GroceryItems.findByIdAndDelete({ _id: req.params.id }).then(
-		(delGroceryItem) => res.status(204).json(delGroceryItem).catch(next)
-	);
+router.delete('/:id', async (req, res, next) => {
+	try {
+		const deleteGroceryItem = await GroceryItems.findByIdAndDelete(req.params.id);
+		res.json(deleteGroceryItem);
+	} catch (err) {
+		next(err);
+	}
 });
-
 module.exports = router;
