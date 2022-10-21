6ebd263a444f2f31b6038750b18f9aa5f29c2805
BrandonBishop84 <brandonnovember@gmail.com>
2022-10-21 12:31:43 -0700

    update index.js

--- index.js
+++ index.js
@@ -3,18 +3,21 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const groceryItemsController = require('./controllers/groceryItemsController');
-const PORT = process.env.PORT || 8000;
+//const PORT = process.env.PORT || 8000;
 //comment
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.get('/', (req, res) => {
+	res.redirect('/grocery-list');
+});
 
 // Controllers
 app.use('/grocery-list', groceryItemsController);
 
-app.set('port', PORT);
+app.set('port', process.env.PORT || 8000);
 
-app.listen(8000, () => {
-	console.log('🛒 cart... start!!! on port:' + PORT);
+app.listen(process.env.PORT || 8000, () => {
+	console.log('🛒 cart... start!!! on port:');
 });
